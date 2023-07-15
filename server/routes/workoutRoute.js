37bcf36d5b3a5eb992d6f5bddc7e052f5d251d03
BrightOlawale <oolawalebright@gmail.com>
2023-07-15 15:03:26 +0100

    style: Improved commenting

--- server/routes/workoutRoute.js
+++ server/routes/workoutRoute.js
@@ -4,10 +4,15 @@ const authHelpers = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+// Get all workouts
 router.route('/').get(authHelpers.authenticateToken, getWorkouts);
+// Get single workout
 router.route('/:id').get(authHelpers.authenticateToken, getOneWorkout);
+// Create workout
 router.route('/').post(authHelpers.authenticateToken, createWorkout);
+// Update workout
 router.route('/:id').put(authHelpers.authenticateToken, updateWorkout);
+// Delete workout
 router.route('/:id').delete(authHelpers.authenticateToken, deleteWorkout);
 
 module.exports = router;    
