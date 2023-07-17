f5c027da64951d3100ae432d70c5d7502ff7e95f
BrightOlawale <oolawalebright@gmail.com>
2023-07-17 23:45:35 +0100

    style: better commenting

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,17 +1,29 @@
 const express = require('express');
-const { getUsers, getUser, createUser, updateUser, deleteUser, loginUser, logoutUser } = require('../controllers/usersController');
+const { getUsers, getUser, createUser, updateUser, deleteUser, loginUser, logoutUser, changePassword, forgotPassword, resetPassword } = require('../controllers/usersController');
 const authHelpers = require('../middleware/authMiddleware');
 
 
 const router = express.Router();
 
-
+// create user
 router.route('/').post(createUser);
-// router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
+// get users
+router.route('/').get(getUsers);
+// login user
 router.route('/login').post(loginUser);
+// get user profile
 router.route('/profile').get(authHelpers.authenticateToken, getUser);
+// update user profile
 router.route('/profile/update').put(authHelpers.authenticateToken, updateUser);
+// delete user profile
 router.route('/profile/delete').delete(authHelpers.authenticateToken, deleteUser);
-router.route('/profile/logout').get(authHelpers.authenticateToken,       logoutUser);
+// logout user
+router.route('/profile/logout').get(authHelpers.authenticateToken, logoutUser);
+//  Change password
+router.route('/profile/change-password').put(authHelpers.authenticateToken, changePassword);
+// Forgot password
+router.route('/forgot-password').post(forgotPassword);
+// Reset password using mailgun
+router.route('/reset-password/:token').post(resetPassword);
 
 module.exports = router;
